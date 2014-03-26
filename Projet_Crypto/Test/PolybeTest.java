c54160396e08f3ff52259d21ea8043d36c04d8f7
nicolasMachut <machut.nicolas@gmail.com>
2014-03-26 17:23:57 +0100

    Ajout d'un test dans polybe

--- Projet_Crypto/Test/PolybeTest.java
+++ Projet_Crypto/Test/PolybeTest.java
@@ -1,5 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
 import project_crypto.Models.Polybe;
+import static org.junit.Assert.*;
 
 /**
  * Created by nicolas on 26/03/14.
@@ -12,5 +14,28 @@ public class PolybeTest {
     public void Setup()
     {
         this.polybe = new Polybe();
+    }
+
+    @Test
+    public void TestPolybeCrypting()
+    {
+        String textToCrypt = "CESTUNTESTEEE";
+
+        //cryptedText must be changed if hashMap association in Polybe class is changed
+        String cryptedText = "13154445513445154445151515";
+
+        this.polybe.Crypting(textToCrypt);
+        assertEquals(cryptedText, this.polybe.GetEncryptedString());
+    }
+
+    @Test
+    public void TestPolybeUncrypting()
+    {
+        String textToUncrypt = "13154445513445154445151515";
+
+        String uncryptedText = "CESTUNTESTEEE";
+
+        this.polybe.Uncrypting(textToUncrypt);
+        assertEquals(uncryptedText, this.polybe.GetUncryptedString());
     }
 }
