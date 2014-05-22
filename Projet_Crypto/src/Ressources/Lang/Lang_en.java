5b46a1054a295b0476f31d2936799cc656acaebf
kimsavinfo <kimsavinfo@gmail.com>
2014-05-22 21:52:38 +0200

    Ajout des ressources pour les champs de texte de l'interface

---
+++ Projet_Crypto/src/Ressources/Lang/Lang_en.java
@@ -0,0 +1,10 @@
+package Ressources.Lang;
+
+/**
+ * Created by kimsavinfo on 22/05/14.
+ */
+public class Lang_en
+{
+    public static String encrypt = "Encrypt";
+    public static String uncrypt = "Uncrypt";
+}
